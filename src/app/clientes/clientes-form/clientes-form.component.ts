e283c61ac26116981ccc779848660a1a64d3023b
Thiago Siqueira dos Santos <thiago.santos@spread.com.br>
2020-12-01 10:05:53 -0300

    Implementado a logica do botão apra salvar o Cliente

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -10,6 +10,8 @@ import { Cliente } from '../cliente';
 export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
+  success: boolean = false;
+  errors: String[] = [];
 
   constructor(private service: ClientesService) { 
     this.cliente = new Cliente();
@@ -21,7 +23,13 @@ export class ClientesFormComponent implements OnInit {
   onSubmit() {
     this.service.salvar(this.cliente)
         .subscribe( response => {
-          console.log(response);
+          this.success = true;
+          this.errors = [];
+          this.cliente = response;
+        }, errorResponse => {
+            this.errors = errorResponse.error.errors;
+            this.success = false;
+            this.cliente = new Cliente();
         });
   }
 
