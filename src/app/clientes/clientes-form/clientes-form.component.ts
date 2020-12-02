1fc62f237889fdb8656ed8d6961ec8cef1dc5ec8
Thiago Siqueira dos Santos <thiago.santos@spread.com.br>
2020-12-02 10:18:03 -0300

    Implementado serviço para atualizar cliente

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { ClientesService } from 'src/app/clientes.service';
 import { Cliente } from '../cliente';
 
@@ -13,19 +14,44 @@ export class ClientesFormComponent implements OnInit {
   cliente: Cliente;
   success: boolean = false;
   errors: String[] = [];
+  id!: number;
 
   constructor(
     private service: ClientesService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute
     ) { 
     this.cliente = new Cliente();
   }
 
   ngOnInit(): void {
+    let params : Observable<Params> = this.activatedRoute.params;
+    params.subscribe( urlParams => {
+      this.id = urlParams['id'];
+      
+      if(this.id){
+        this.service
+          .getClientePorId(this.id)
+          .subscribe( 
+            response => this.cliente = response,
+            errorResponse => this.cliente = new Cliente()
+          );
+      }
+    });
   }
 
   onSubmit() {
-    this.service.salvar(this.cliente)
+    if(this.id){
+      this.service.atualizar(this.cliente)
+          .subscribe( response => {
+            this.success = true;
+            this.errors = [];
+          }, errorResponse => {
+            this.errors = ['Erro ao tentar atualizar o ciente.']
+            this.success = false;
+          })
+    }else {
+      this.service.salvar(this.cliente)
         .subscribe( response => {
           this.success = true;
           this.errors = [];
@@ -34,6 +60,7 @@ export class ClientesFormComponent implements OnInit {
             this.errors = errorResponse.error.errors;
             this.success = false;
         });
+    }
   }
 
   voltarListagem() {
