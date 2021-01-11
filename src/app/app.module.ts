bf3722f08a67a27eb4fecf5e60f310d5f44470f5
Thiago Siqueira dos Santos <thiago.santos@spread.com.br>
2021-01-11 10:45:29 -0300

    Implementada tela para cadastro de usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ServicoService } from './servico.service';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { LayoutComponent } from './layout/layout.component';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   providers: [
     ClientesService, 
-    ServicoService
+    ServicoService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +12,13 @@ export class LoginComponent {
   
   username!: string;
   password!: string;
-  loginError!: boolean;
+  errors!: string[];
   cadastrando!: boolean;
+  mensagemSucesso!: string;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   onSubmit() {
@@ -28,5 +32,19 @@ export class LoginComponent {
 
   cancelaCadastro() {
     this.cadastrando = false;
+  }
+
+  cadastrar() {
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
+
+    this.authService.salvar(usuario)
+                    .subscribe( response => {
+                        this.mensagemSucesso = "Cadastro realizado com sucesso!";
+                    }, errorResponse => {
+                        this.errors = errorResponse.error.errors;
+                        this.mensagemSucesso = "";
+                    });
   }
 }
